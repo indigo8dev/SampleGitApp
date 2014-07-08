dd8694fc310026503667ef183d69cb21cfc08ea0
deniece gozon <deniece_gozon@indigo8-solutions.com>
2014-07-08 15:27:05 +0800

    test

---
+++ samplegitapp/samplegitapp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  samplegitapp
+//
+//  Created by HaroldMac on 7/8/14.
+//  Copyright (c) 2014 Indigo8 Philippines Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ samplegitapp/samplegitappTests/samplegitappTests.m
@@ -0,0 +1,34 @@
+//
+//  samplegitappTests.m
+//  samplegitappTests
+//
+//  Created by HaroldMac on 7/8/14.
+//  Copyright (c) 2014 Indigo8 Philippines Inc. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface samplegitappTests : XCTestCase
+
+@end
+
+@implementation samplegitappTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
